ecfd55b83cce7ca16981d3a73adf33c511e57882
清雪冰橙 <1154238323@qq.com>
2020-04-08 23:44:05 +0800

    接入层对象

---
+++ backend/project/src/main/java/response/CommonReturnType.java
@@ -0,0 +1,30 @@
+package response;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * @author czf
+ * @Date 2020/4/8 11:31 下午
+ */
+@Data
+public class CommonReturnType {
+    // state 取值范围 { "success", "fail" }
+    private String status;
+    // 若state为success, 则data为返回结果
+    // 若state为fail，则data内使用通用的错误码格式
+    private Object data;
+
+    // 创建一个通用的创建方法
+    public static CommonReturnType create(Object result){
+        return CommonReturnType.create(result, "success");
+    }
+
+    public static CommonReturnType create(Object result, String status) {
+        CommonReturnType type = new CommonReturnType();
+        type.setData(result);
+        type.setStatus(status);
+        return type;
+    }
+}

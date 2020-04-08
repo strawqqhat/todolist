27868b4ddc2ee0e5602f3d87b464822979a23038
清雪冰橙 <1154238323@qq.com>
2020-04-08 23:25:29 +0800

    异常处理

---
+++ backend/project/src/main/java/error/CommonError.java
@@ -0,0 +1,11 @@
+package error;
+
+/**
+ * @author czf
+ * @Date 2020/4/9 3:27 下午
+ */
+public interface CommonError {
+    public int getErrCode();
+    public String getErrMsg();
+    public CommonError setErrMsg(String errMsg);
+}

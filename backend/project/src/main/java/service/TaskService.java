338a83d7f1752d567baf76564e27d2e16e9ff2ad
清雪冰橙 <1154238323@qq.com>
2020-04-08 23:53:07 +0800

    TaskService接口及其实现

---
+++ backend/project/src/main/java/service/TaskService.java
@@ -0,0 +1,11 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author czf
+ * @Date 2020/4/8 11:49 下午
+ */
+public interface TaskService {
+
+}
---
+++ backend/project/src/main/java/service/impl/TaskServiceImpl.java
@@ -0,0 +1,10 @@
+package service.impl;
+
+import service.TaskService;
+
+/**
+ * @author czf
+ * @Date 2020/4/8 11:52 下午
+ */
+public class TaskServiceImpl implements TaskService {
+}

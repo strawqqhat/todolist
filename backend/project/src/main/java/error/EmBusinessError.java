1f823a028740cedeaa539f9b58bdc06efa2e7e76
清雪冰橙 <1154238323@qq.com>
2020-04-08 23:29:02 +0800

    异常处理

--- backend/project/src/main/java/error/EmBusinessError.java
+++ backend/project/src/main/java/error/EmBusinessError.java
@@ -8,16 +8,6 @@ public enum EmBusinessError implements CommonError {
     // 通用错误类型
     PARAMETER_VALIDATION_ERROR(10001,"参数不合法"),
     UNKNOWN_ERROR(10002,"未知错误"),
-
-    // 2开头为用户信息相关错误定义
-    USER_NOT_EXIST(20001,"用户不存在"),
-    USER_LOGIN_FAIL(20002, "登陆失败"),
-    USER_NOT_LOGIN(20003, "用户还未登陆"),
-    HAS_REGISTERED(20004,"该手机号已注册"),
-    // 3开头为交易信息错误定义
-    STOCK_NOT_ENOUGH(30001,"库存不足!"),
-    MQ_SEND_ERROR(30002, "库存异步消息发送失败")
-
     ;
 
     private int errCode;

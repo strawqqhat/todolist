8631b38465be3e4acb3e3d39c5cef6f5fe8eb2c1
清雪冰橙 <1154238323@qq.com>
2020-04-09 15:01:37 +0800

    给taskName增加唯一索引

--- backend/mysql/create.sql
+++ backend/mysql/create.sql
@@ -6,7 +6,9 @@ create table `task_info`(
     `id` int(11) not null auto_increment,
     `task_name` varchar(255) not null,
     `description` varchar(255) not null DEFAULT '无',
-    `deadline` datetime not null DEFAULT '0000-01-01 00:00:00',
+    `deadline` datetime not null DEFAULT '1970-01-01 00:00:00',
     `finished` int not null DEFAULT -1,
     primary key(`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+alter table `task_info` add unique(`task_name`);

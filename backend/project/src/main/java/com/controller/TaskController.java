5671bd6b7594dcf0331d2ff8ff1f3b0abfc96b71
清雪冰橙 <1154238323@qq.com>
2020-04-09 09:42:15 +0800

    增加时间类型格式是否合法的判断

--- backend/project/src/main/java/com/controller/TaskController.java
+++ backend/project/src/main/java/com/controller/TaskController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import com.error.BusinessException;
+import com.error.EmBusinessError;
 import com.response.CommonReturnType;
 import com.service.TaskService;
 import com.service.model.TaskModel;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 /**
@@ -56,7 +60,7 @@ public class TaskController extends BaseController {
                                        @RequestParam(value = "taskName",required = true)String taskName,
                                        @RequestParam(value = "description", required = true)String description,
                                        @RequestParam(value = "deadline", required = false)String deadline,
-                                       @RequestParam(value = "finished", required = true)Integer finished){
+                                       @RequestParam(value = "finished", required = true)Integer finished) throws BusinessException {
         TaskModel taskModel = new TaskModel();
         taskModel.setDescription(description);
         taskModel.setTaskName(taskName);
@@ -64,10 +68,27 @@ public class TaskController extends BaseController {
         taskModel.setId(id);
 
         if (deadline!=null) {
+            if ( !isDate(deadline) )
+                throw new BusinessException(EmBusinessError.FORMAT_ERROR,"时间类型格式错误！");
             DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
-            taskModel.setDeadline(DateTime.parse(deadline,formatter));
+            taskModel.setDeadline(DateTime.parse(deadline, formatter));
         }
         taskService.modifyTask(taskModel);
         return CommonReturnType.create(null);
+    }
+
+    /**
+     * 检测时间类型是否合法
+     * @param date
+     * @return
+     */
+    private boolean isDate(String date){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            format.parse(date);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
     }
 }

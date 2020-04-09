e865da9b146d46ad267d5869779b39efc647ccad
清雪冰橙 <1154238323@qq.com>
2020-04-09 18:10:00 +0800

    重载了deleteTask方法，可以用多种方式删除任务

--- backend/project/src/main/java/com/service/TaskService.java
+++ backend/project/src/main/java/com/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.service;
 
+import com.error.BusinessException;
 import com.service.model.TaskModel;
 
 import java.util.List;
@@ -19,13 +20,19 @@ public interface TaskService {
      * 增加一个任务
      * @param taskModel
      */
-    void addTask(TaskModel taskModel);
+    void addTask(TaskModel taskModel) throws BusinessException;
 
     /**
      * 根据id删除任务
      * @param id
      */
     void deleteTask(Integer id);
+
+    /**
+     * 根据taskName删除任务
+     * @param taskName
+     */
+    void deleteTask(String taskName);
 
     /**
      * 修改task

9c1416b47c489ff54bc11a9e670a01ff763a9a20
strawqqhat <885228764@qq.com>
2020-04-15 21:58:16 +0800

    返回所有Todo任务的测试

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -1,7 +1,18 @@
-describe('sample test', function () {
-    it('should work', async function () {
-        console.log(await browser.version());
-
-        expect(true).to.be.true;
+describe('Todo List', function () {
+    let page;
+    before (async function () {
+        page = await browser.newPage();
+        await page.goto('http://127.0.0.1:3000');
     });
+
+    after (async function () {
+        await page.close();
+    });
+    describe('get all tasks', function() {
+        it('should return all tasks correctly', async function () {
+            let todoList = await page.$$('#root > div > div > li', input=>input.value);
+            console.log(todoList.length);
+        });
+    });
+    
 });

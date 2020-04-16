c1f87473ce87844acba9a826fe3cf62ee9dee23c
strawqqhat <885228764@qq.com>
2020-04-16 12:48:41 +0800

    删除任务的测试

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -15,18 +15,31 @@ describe('Todo List', function () {
         });
     });
 
-    describe('add task', function() {
+    describe('add a new task', function() {
         it('should add a new task correctly', async function(){
             let todoList = await page.$$('#root > div > div > li');
             const len1 = todoList.length;
-            await page.click('input[type=text]', {delay: 500});
-            await page.type('input[type=text]', 'the ewwwww', {delay: 1000});
-            await page.click('button[data-test-id=add-button]');
+            await page.click('.task-input', {delay: 500});
+            await page.type('.task-input', 'the ewwwww', {delay: 500});
+            await page.click('.submit');
             // 之前没有设置等待时间总是导致新录入信息没加载出来就去获取，得不到新数据
             await page.waitFor(1000);
             let new_todoList = await page.$$('#root > div > div > li');
             const len2 = new_todoList.length;
             expect (len2-len1).to.eql(1);
+        });
+    });
+
+    describe('delete a task', function() {
+        it('should delete a task correctly', async function() {
+            let todoList = await page.$$('#root > div > div > li');
+            const len1 = todoList.length;
+            await page.evaluate(()=> {
+                document.querySelector('.delete').click()
+            });
+            await page.waitFor(1000);
+            const len2 = await page.$$eval('#root > div > div > li', list=>list.length);
+            expect (len1-len2).to.eql(1);
         });
     });
     

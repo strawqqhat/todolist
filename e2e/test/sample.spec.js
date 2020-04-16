4e1d78935f4beee3babd2e0a7f9a955119d78e20
strawqqhat <885228764@qq.com>
2020-04-16 12:59:41 +0800

    添加任务功能的改进

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -1,7 +1,11 @@
 describe('Todo List', function () {
     let page;
+    let newTaskContent;
+
     before (async function () {
         page = await browser.newPage();
+        let random = new Date().getMilliseconds();
+        newTaskContent = 'new task item' + random;
         await page.goto('http://127.0.0.1:3000');
     });
 
@@ -17,30 +21,29 @@ describe('Todo List', function () {
 
     describe('add a new task', function() {
         it('should add a new task correctly', async function(){
-            let todoList = await page.$$('#root > div > div > li');
-            const len1 = todoList.length;
+
+            const len1 = await page.$$eval('#root > div > div > li', list=>list.length);
             await page.click('.task-input', {delay: 500});
-            await page.type('.task-input', 'the ewwwww', {delay: 500});
+            await page.type('.task-input', newTaskContent, {delay: 500});
             await page.click('.submit');
             // 之前没有设置等待时间总是导致新录入信息没加载出来就去获取，得不到新数据
             await page.waitFor(1000);
-            let new_todoList = await page.$$('#root > div > div > li');
-            const len2 = new_todoList.length;
+            const len2 = await page.$$eval('#root > div > div > li', list=>list.length);
             expect (len2-len1).to.eql(1);
         });
     });
 
-    describe('delete a task', function() {
-        it('should delete a task correctly', async function() {
-            let todoList = await page.$$('#root > div > div > li');
-            const len1 = todoList.length;
-            await page.evaluate(()=> {
-                document.querySelector('.delete').click()
-            });
-            await page.waitFor(1000);
-            const len2 = await page.$$eval('#root > div > div > li', list=>list.length);
-            expect (len1-len2).to.eql(1);
-        });
-    });
+    // describe('delete a task', function() {
+    //     it('should delete a task correctly', async function() {
+    //         let todoList = await page.$$('#root > div > div > li');
+    //         const len1 = todoList.length;
+    //         await page.evaluate(()=> {
+    //             document.querySelector('.delete').click()
+    //         });
+    //         await page.waitFor(1000);
+    //         const len2 = await page.$$eval('#root > div > div > li', list=>list.length);
+    //         expect (len1-len2).to.eql(1);
+    //     });
+    // });
     
 });

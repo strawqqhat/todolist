2abfa454f670d253f318bad9b67d1cd5212fbbc0
strawqqhat <885228764@qq.com>
2020-04-16 16:29:34 +0800

    添加任务测试的再次改进

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -22,15 +22,17 @@ describe('Todo List', function () {
     describe('add a new task', function() {
         it('should add a new task correctly', async function(){
 
-            const len1 = await page.$$eval('#root > div > div > li', list=>list.length);
             await page.click('.task-input', {delay: 500});
             await page.type('.task-input', newTaskContent, {delay: 500});
             await page.click('.submit');
             // 之前没有设置等待时间总是导致新录入信息没加载出来就去获取，得不到新数据
             await page.waitFor(1000);
+            // 每个li标签包含一个任务项，:last-child选取最后一个任务项，也就是新增的任务项
             let newTask = await page.waitFor('#root > div > div > li:last-child');
+            // 使用querySelector找到input标签，新增任务项的内容在input标签中的value属性里
             const expectNewTask = await page.evaluate(task=>task.querySelector('input').value, newTask);
             expect (expectNewTask).to.eql(newTaskContent);
+            
         });
     });
 

7fd4994c5a861bdee06b376e8fcaacb0a5a53bb9
strawqqhat <885228764@qq.com>
2020-04-16 08:14:36 +0800

    新增任务的测试

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -10,8 +10,23 @@ describe('Todo List', function () {
     });
     describe('get all tasks', function() {
         it('should return all tasks correctly', async function () {
-            let todoList = await page.$$('#root > div > div > li', input=>input.value);
+            let todoList = await page.$$('#root > div > div > li');
             console.log(todoList.length);
+        });
+    });
+
+    describe('add task', function() {
+        it('should add a new task correctly', async function(){
+            let todoList = await page.$$('#root > div > div > li');
+            const len1 = todoList.length;
+            await page.click('input[type=text]', {delay: 500});
+            await page.type('input[type=text]', 'the ewwwww', {delay: 1000});
+            await page.click('button[data-test-id=add-button]');
+            // 之前没有设置等待时间总是导致新录入信息没加载出来就去获取，得不到新数据
+            await page.waitFor(1000);
+            let new_todoList = await page.$$('#root > div > div > li');
+            const len2 = new_todoList.length;
+            expect (len2-len1).to.eql(1);
         });
     });
     
